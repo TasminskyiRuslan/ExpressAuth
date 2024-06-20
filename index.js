df1d7873cfadb849ab2f7ee62ced60b3d44e70a9
TasminskyiRuslan <tasminskyi.ruslan@gmail.com>
2024-06-20 15:20:21 +0300

    create project

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 import express from "express"
-import mongoose from "mongoose"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 import "dotenv/config"
 
 const PORT = process.env.PORT || 5000
+
 const app = express()
 
 app.use(cors)
 app.use(cookieParser)
 
-const start = () => {
-    app.listen(PORT, () => console.log(`Server has been started on http://localhost:${PORT}`))
+const start = async () => {
+    try {
+        app.listen(PORT, () => console.log(`Server has been started on ${process.env.API_URL}:${PORT}`))
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 start()
